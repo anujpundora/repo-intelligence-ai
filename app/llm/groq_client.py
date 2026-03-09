091a5367e4497a37c03f77cb24e441c5848b88d4
Anuj pundora <anuj28279@gmail.com>
2026-03-09 11:38:31 +0530

    Updated groq Model with latest and instant to reduce latency

--- app/llm/groq_client.py
+++ app/llm/groq_client.py
@@ -13,7 +13,7 @@ def generate(prompt: str):
         messages=[
             {"role": "user", "content": prompt}
         ],
-        model="llama3-8b-8192"
+        model="llama-3.1-8b-instant"
     )
 
     return chat_completion.choices[0].message.content

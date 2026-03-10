2abe0e2591975bc7e3cefd938d2aa427f9289550
Anuj pundora <anuj28279@gmail.com>
2026-03-10 23:23:57 +0530

    Added shared memory(CONTEXT)  between agents

--- app/agents/agent_state.py
+++ app/agents/agent_state.py
@@ -3,8 +3,17 @@ class AgentState:
     def __init__(self, task):
 
         self.task = task
+
         self.history = []
         self.observations = []
+
+        # shared memory between agents
+        self.context = {
+            "retrieved_chunks": [],
+            "security_findings": [],
+            "bug_findings": [],
+            "analysis_summary": []
+        }
 
     def add_step(self, action, result):
 
